7648a6197945aa0310bf068f09896db121ce9eae
lone1031 <lone1031@users.noreply.github.com>
2023-07-12 16:14:45 +0800

    23.7.12 new：Variable

---
+++ Variable.py
@@ -0,0 +1,13 @@
+# 定义一个变量，用来记录钱包余额的值
+WalletBalance = 50
+# 通过print语句，输出变量记录的内容
+print("钱包余额为：", WalletBalance, "元")
+
+WalletBalance = WalletBalance - 10
+print("钱包余额为：", WalletBalance, "元")
+
+# 假设：每隔一小时，输出钱包余额
+print("现在是下午1点，钱包余额为", WalletBalance, "元")
+print("现在是下午2点，钱包余额为", WalletBalance, "元")
+print("现在是下午3点，钱包余额为", WalletBalance, "元")
+print("现在是下午4点，钱包余额为", WalletBalance, "元")
